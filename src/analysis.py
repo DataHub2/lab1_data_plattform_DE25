a302ddb4c9d6dd6c05971fc67804f35b9def249d
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-02-16 18:14:37 +0100

    feat i succsessfully was able to read the file and got a count of 55 rows, i also fixed some typos. I will now be working on cleaning the data

--- src/analysis.py
+++ src/analysis.py
@@ -5,17 +5,35 @@ import os
 INPUT_FILE = "data/raw/lab 1 - csv.csv"
 # Here i  i want to save the processed file in the processed folder.
 OUTPUT_DIR = "data/processed/"
-SUMMORY_FILE = os.path.join(OUTPUT_DIR,"analytics_summary.csv")
+# File name for the file that i am going to save.
+SUMMARY_FILE = os.path.join(OUTPUT_DIR,"analytics_summary.csv")
 TOP_PRICES_FILE = os.path.join(OUTPUT_DIR,"price_analysis.csv")
 
 def main():
-    # Reading the file
-    # I first want to check if the file exists.
-    if os.path.exists(INPUT_FILE):
-        print(f"Error: The file {INPUT_FILE} does not exist.")
-        return
-    print(f"Reading the file: {INPUT_FILE}")
+    print("Starting lab")
     
+    # Reading the file
+    print(f"Reading file {INPUT_FILE}")
+
+    # I checked the file and saw that it is separated by semicolons, so i implemeted the sep parameter to read the file correctly.
+    df = pd.read_csv(INPUT_FILE, sep=";")
+    
+    print(f" Found {len(df)} rows")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+    
+
 
                                
                         
